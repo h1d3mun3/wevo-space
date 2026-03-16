b8136bd9ee192bf42bbd68095580741cb2df05f8
Hidemune Takahashi <h1d3mun3@gmail.com>
2026-03-16 20:16:09 +0900

    add timestamp fields to initial migrations
    CreateProposesTable / CreateCounterpartiesTable にタイムスタンプカラムを追加。

--- Sources/WevoSpace/Migrations/CreateCounterpartiesTable.swift
+++ Sources/WevoSpace/Migrations/CreateCounterpartiesTable.swift
@@ -7,8 +7,11 @@ struct CreateCounterpartiesTable: AsyncMigration {
             .field("propose_id", .uuid, .required, .references("proposes", "id", onDelete: .cascade))
             .field("public_key", .string, .required)
             .field("sign_signature", .string)
+            .field("sign_timestamp", .string)
             .field("honor_signature", .string)
+            .field("honor_timestamp", .string)
             .field("part_signature", .string)
+            .field("part_timestamp", .string)
             .create()
     }
 
